ea1bcd9f626a0b78ec1f20e51443b70ae32139c5
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2025-03-07 11:08:27 -0500

    fixes test url

--- automationtests/helpers/searchData.js
+++ automationtests/helpers/searchData.js
@@ -65,7 +65,7 @@ module.exports = {
   condoAddressData,
   pwdAddressData,
   dorAddressData,
-  URL:"https://d1dycpzd1ntl2z.cloudfront.net/",
+  URL:"https://atlas-dev.phila.gov/",
   HEADLESS_MODE: true,
   BROWSER:"chrome",
 };
