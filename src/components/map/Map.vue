429250abb793a2f8da0143ba34c5db83a3ee0680
Theron Halsey <theron.halsey@phila.gov>
2025-12-23 10:07:34 -0500

    comment for that -10 thing

--- src/components/map/Map.vue
+++ src/components/map/Map.vue
@@ -541,7 +541,7 @@ const toggleImagery = () => {
   if (!MapStore.imageryOn) {
     MapStore.imageryOn = true;
     // if a year has already been selected, use that as the current year. If not, then get the more recent year with a satelite map from the mapLayers
-    const currentYear = imagerySelected.value ? imagerySelected.value : Object.keys($config.mapLayers).at(-10);
+    const currentYear = imagerySelected.value ? imagerySelected.value : Object.keys($config.mapLayers).at(-10); // -10 index should always point to most recent year provided the number of layers with alphabet character names does not change
     setImagery(currentYear);
   }
   // if imagery is going from 'on' to 'off'
