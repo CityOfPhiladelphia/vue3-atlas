0d6e25ca21ecfc51f64b17f55d0801e6ad8a06a4
rothwell.andy@gmail.com <rothwell.andy@gmail.com>
2026-01-15 12:32:04 -0500

    adds st_contains and uses union with overlaps

--- src/stores/ZoningStore.js
+++ src/stores/ZoningStore.js
@@ -127,11 +127,15 @@ export const useZoningStore = defineStore('ZoningStore', {
           const query = "WITH all_proposed_zoning AS ( SELECT * FROM phl.proposedzoning_imp_public ), \
             parcel AS \
               ( SELECT * FROM phl.dor_parcel WHERE dor_parcel.mapreg = '" + mapreg + "' ), \
-              zp_intersects AS \
-              ( SELECT all_proposed_zoning.* FROM all_proposed_zoning, parcel WHERE st_intersects(parcel.the_geom, all_proposed_zoning.the_geom)) \
-              SELECT * FROM zp_intersects";
-          // zp_overlaps AS \
-          //   ( SELECT all_proposed_zoning.* FROM all_proposed_zoning, parcel WHERE st_overlaps(parcel.the_geom, all_proposed_zoning.the_geom)), \
+              zp_overlaps AS \
+                ( SELECT all_proposed_zoning.* FROM all_proposed_zoning, parcel WHERE st_overlaps(parcel.the_geom, all_proposed_zoning.the_geom)), \
+              zp_contains AS \
+                ( SELECT all_proposed_zoning.* FROM all_proposed_zoning, parcel WHERE st_contains(all_proposed_zoning.the_geom, parcel.the_geom)), \
+              SELECT * from zp_overlaps UNION SELECT * from zp_contains";
+
+              // zp_intersects AS \
+              // ( SELECT all_proposed_zoning.* FROM all_proposed_zoning, parcel WHERE st_intersects(parcel.the_geom, all_proposed_zoning.the_geom)) \
+              // SELECT * FROM zp_intersects";
           
           // const query = "WITH all_proposed_zoning AS ( SELECT * FROM phl.proposedzoning_imp_public ), \
           //   parcel AS \
