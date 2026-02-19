b7f3a6754b30d2c6ba18122250c9df058d588fcd
Theron Halsey <theron.halsey@phila.gov>
2026-02-19 13:15:40 -0500

    error for eagleview token failure

--- src/components/map/EagleviewPanel.vue
+++ src/components/map/EagleviewPanel.vue
@@ -92,46 +92,49 @@ watch(
 
 onMounted(async () => {
   MapStore.eagleviewToken = MapStore.eagleviewToken ? MapStore.eagleviewToken : await getEagleviewToken();
-  const config = {
-    authToken: MapStore.eagleviewToken,
-    measurementPanelEnabled: false,
-    searchBarEnabled: false,
-    enableDualPaneButton: false,
-    view: {
-      lonLat: currentAddressCoords.value,
-      zoom: 17,
-      pitch: 0,
-      rotation: 0
-    },
-    preferences: {
-      imageDisplay: {
-        'mainPane': 'oblique'
-      }
-    }
-  }
-
-  map = new window.ev.EmbeddedExplorer().mount('eagleview', config);
-  if (MapStore.currentAddressCoords.length) {
-    map.addFeatures({
-      geoJson: [
-        point(
-          [currentAddressCoords.value.lon, currentAddressCoords.value.lat],
-          eagleviewProperties
-        )
-      ]
-    });
-  }
-
-  map.getLayers();
-  map.on('onLayersDataLoad', () => {
-    map.updateLayers(
-      {
-        filter: (layer) => {
-          layer.visible = false;
+  if (MapStore.eagleviewToken) {
+    const config = {
+      authToken: MapStore.eagleviewToken,
+      measurementPanelEnabled: false,
+      searchBarEnabled: false,
+      enableDualPaneButton: false,
+      view: {
+        lonLat: currentAddressCoords.value,
+        zoom: 17,
+        pitch: 0,
+        rotation: 0
+      },
+      preferences: {
+        imageDisplay: {
+          'mainPane': 'oblique'
         }
       }
-    );
-  });
+    }
+
+    map = new window.ev.EmbeddedExplorer().mount('eagleview', config);
+    if (MapStore.currentAddressCoords.length) {
+      map.addFeatures({
+        geoJson: [
+          point(
+            [currentAddressCoords.value.lon, currentAddressCoords.value.lat],
+            eagleviewProperties
+          )
+        ]
+      });
+    }
+
+    map.getLayers();
+    map.on('onLayersDataLoad', () => {
+    map.updateLayers({
+      filter: (layer) => {
+        layer.visible = false;
+        }
+      });
+    });
+  }
+  else {
+    throw new Error("Failed to get access token for EagleView")
+  }
 });
 
 const popoutClicked = () => {
