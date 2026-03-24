65038fda09a74380dd912df593cba78a3e36cada
Theron Halsey <theron.halsey@phila.gov>
2026-03-24 12:52:33 -0400

    changed endpoints

--- src/components/map/CyclomediaPanel.vue
+++ src/components/map/CyclomediaPanel.vue
@@ -14,8 +14,6 @@ const cyclomedia = useCyclomedia();
 
 import $config from '@/config';
 
-
-
 const cyclomediaInitialized = ref(false);
 const streetView = useTemplateRef('cycloviewer')
 
@@ -94,6 +92,9 @@ const setNewLocation = async (coords) => {
       if (import.meta.env.VITE_DEBUG == 'true') console.log('update cyclomedia date, viewer.props.recording.year:', viewer.props.recording.year);
       MapStore.cyclomediaYear = viewer.props.recording.year;
       // $emit('updateCyclomediaDate', e.recording.year);
+      const orientation = viewer.getOrientation();
+      viewer.setOrientation({ pitch: 0 });
+      if (import.meta.env.VITE_DEBUG == 'true') console.log('orientation:', orientation);
       if (viewer.props.orientation.xyz !== MapStore.cyclomediaCameraXyz) {
         const lngLat = [viewer.props.orientation.xyz[0], viewer.props.orientation.xyz[1]];
         MapStore.setCyclomediaCameraLngLat(lngLat, viewer.props.orientation.xyz);
